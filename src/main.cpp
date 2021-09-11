3f0c941055285b6a9a7f3fbfb1f77afc8b0f2faf
Shawn Ray <Asmodasis@gmail.com>
2021-09-10 20:47:53 -0700

    Working on ImageSampling, issue with image copy

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+//#include "../include/image.h"
+#include "../include/image.h"
+#include "../include/ImageSampling.h"
+/*
+#include "../include/WriteImage.h"
+int readImage(char fname[], ImageType& image);
+int readImageHeader(char fname[], int& N, int& M, int& Q, bool& type);
+int writeImage(char fname[], ImageType& image);
+*/
+int main(int argc, char *argv[]){
+
+    std::cout << "Main called..." << std::endl;
+
+    int N = 256;
+    int M = 256;
+    int Q = 255;
+
+    ImageType img(N,M,Q);
+
+    imageSampling("../images/lenna.pgm", img, 2);
+
+}
